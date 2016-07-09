6ca4b173a49261f77813e99424885706d52fd391
wesley_custom <darkfrontcode@gmail.com>
2016-07-09 11:08:29 -0300

    adjustments in package

--- cucumber/steps/my_feature.js
+++ cucumber/steps/my_feature.js
@@ -1,11 +1,14 @@
 module.exports = function () {
 
     this.Given(/^I am on the Cucumber\.js GitHub repository$/, function() {
-        return this.visit('https://angularjs.org/')
+        browser.ignoreSynchronization = true;
+        return this.visit('https://github.com/cucumber/cucumber-js')
     })
 
-    this.When(/^I go to the README file$/, function(callback) {
-        callback(null, 'pending')
+    this.When(/^I go to the README file$/, {timeout: 60 * 1000}, function(callback) {
+        let div = browser.findElement(by.css('[title=README.md]')).click()
+
+        // README.md
     })
 
     this.Then(/^I should see "([^"]*)" as the page title$/, function(title, callback) {
@@ -13,11 +16,11 @@ module.exports = function () {
         // browser.wait(EC.presenceOf($('#abc')), 5000);
         // this.getUrl()
 
-        let pageTitle = this.getTitle();
-        if (title === pageTitle)
-            callback()
-        else
-            callback(new Error("Expected title " + title))
+        // let pageTitle = this.getTitle();
+        // if (title === pageTitle)
+        //     callback()
+        // else
+        //     callback(new Error("Expected title " + title))
     })
 
 }
